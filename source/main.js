614e48942586d70325e46b2aadd87be0e8539e7a
Anadian <supercamman20@gmail.com>
2017-08-27 11:44:45 -0400

    Initial Commit.

---
+++ source/main.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+/**
+*	@file source/main.js
+*	@brief
+*	@author Anadian
+*	@copyright MIT License
+*/
+const FileSystem = require('fs');
+const log4js = require('log4js');
+const CommandLineArgs = require('command-line-args');
+const CommandLineCommands = require('command-line-commands');
+const DirectoryTree = require('directory-tree');
+const Request = require('request');
+
